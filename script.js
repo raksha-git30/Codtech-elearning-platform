9b39db6423a3a68d7002d6b68839e3d33d663d99
raksha-git30 <rakshashrimanwar811@gmail.com>
2025-07-20 18:32:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// js/script.js
+
+function markComplete() {
+  alert("Course marked as completed!");
+  // Save progress in localStorage (optional)
+  localStorage.setItem("js-course-completed", "true");
+}
